e4eb76431cd4207401bc07f8f252c9d7ada90cfe
Ol <holmate@dontsp.am>
2025-03-23 18:07:20 +0200

    The finished function-decorator has been added

--- app/main.py
+++ app/main.py
@@ -2,5 +2,29 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    """
+    This decorator caches the result of a function
+    :param func: original function
+    :return: wrapped function
+    """
+    cache_data = {}
+
+    def wrapper(*args, **kwargs):
+        """
+        This wrapper will cache the result of a function
+        :param args: args
+        :param kwargs: kwargs
+        :return: Calculation data or Cached data
+        """
+        key = args + tuple(sorted(kwargs.items()))
+
+        if key not in cache_data:
+            print("Calculation new result")
+            result = func(*args, **kwargs)
+            cache_data[key] = result
+            return result
+        else:
+            print("Getting from cache")
+            return cache_data[key]
+
+    return wrapper
